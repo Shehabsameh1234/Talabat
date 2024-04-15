ddad62bca076a5cccfd19c191ded7852981851c9
shehabsameh1234 <shehabsameh987123@gmail.com>
2024-04-15 18:56:09 +0200

    specification design pattern

---
+++ Talabat.Repository/SpecificationsEvaluetor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Specifications;
+
+namespace Talabat.Repository
+{
+    internal class SpecificationsEvaluetor<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuey,ISepcifications<TEntity> spec)
+        {
+            var query = inputQuey; //_dbContext.Set<T>()
+
+            if (spec.Criteria is not null)
+                query = query.Where(spec.Criteria);
+
+            query = spec.Includes.Aggregate(query, (currentQuery, includeExpression) => currentQuery.Include(includeExpression));
+
+            return query;
+
+        }
+    }
+}
