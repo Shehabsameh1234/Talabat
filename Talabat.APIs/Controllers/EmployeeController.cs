40f87196b936210964ad821f63a967f6f2547ffb
shehabsameh1234 <shehabsameh987123@gmail.com>
2024-04-16 17:34:17 +0200

    Employee Example-Draft

---
+++ Talabat.APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repository.Contract;
+using Talabat.Core.Specifications.productSpecifications;
+
+namespace Talabat.APIs.Controllers
+{
+ 
+    public class EmployeeController : BaseApiController
+    {
+        private readonly IGenericRepository<Emoloyee> _empRepository;
+
+        public EmployeeController(IGenericRepository<Emoloyee> empRepository)
+        {
+           _empRepository = empRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Emoloyee>>> GetEmps()
+        {
+            var spec =new EmployeeWithDepartmentSpecifications();
+            var emps =await _empRepository.GetAllWithSpecAsync(spec);
+            return Ok(emps);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Emoloyee>> GetEmp(int id)
+        {
+            var spec = new EmployeeWithDepartmentSpecifications(id);
+
+            var emp = await _empRepository.GetWithSpecAsync(spec);
+
+            if(emp == null) { return NotFound(); }
+
+            return Ok(emp);
+        }
+    }
+}
---
+++ Talabat.Core/Specifications/productSpecifications/EmployeeWithDepartmentSpecifications.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Specifications.productSpecifications
+{
+    public class EmployeeWithDepartmentSpecifications:BaseSpecifications<Emoloyee>
+    {
+        public EmployeeWithDepartmentSpecifications()
+            :base()
+        {
+            Includes.Add(e => e.Department);
+        }
+        public EmployeeWithDepartmentSpecifications(int id)
+           : base(p=>p.id==id)
+        {
+            Includes.Add(e => e.Department);
+        }
+    }
+}
