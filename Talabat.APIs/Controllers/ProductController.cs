5362e9dc15e0223cd62e0655de2ed095c7c042f8
shehabsameh1234 <shehabsameh987123@gmail.com>
2024-04-17 00:12:15 +0200

    improving swagger documentation

--- Talabat.APIs/Controllers/ProductController.cs
+++ Talabat.APIs/Controllers/ProductController.cs
@@ -29,7 +29,10 @@ namespace Talabat.APIs.Controllers
 			var products = await _productRepository.GetAllWithSpecAsync(spec);
 			return Ok(_mapper.Map<IEnumerable<Product>,IEnumerable<ProductToReturnDto>>(products));
 		}
-		[HttpGet("id")]
+		[ProducesResponseType(typeof(ProductToReturnDto),StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApisResponse), StatusCodes.Status404NotFound)]
+
+        [HttpGet("id")]
 		public async Task<ActionResult<IEnumerable<ProductToReturnDto>>> GetProduct(int id)
 		{
             var spec = new ProductWithBrandAndCategorySpecifications(id);
