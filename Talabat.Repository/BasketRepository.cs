d441f38c840f956fa03a820516488d4e6decd8fb
shehabsameh1234 <shehabsameh987123@gmail.com>
2024-04-23 13:29:34 +0200

    basket repo

---
+++ Talabat.Repository/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Talabat.Core.Entities;
+using Talabat.Core.Repository.Contract;
+
+namespace Talabat.Repository
+{
+    public class BasketRepository : IBasektRepository
+    {
+        private readonly IDatabase _database;
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+            _database=redis.GetDatabase();
+        }
+        public async Task<bool> DeleteBasketAsync(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
+        }
+
+        public async Task<CustomerBasket?> GetBasketAsync(string basketId)
+        {
+            //struct =>value cant be null
+            var basket= await _database.StringGetAsync(basketId);
+            return basket.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(basket);
+        }
+
+        public async Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket)
+        {
+          var isCreatedOrUpdate=  await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+            if (!isCreatedOrUpdate) return null;
+            return await GetBasketAsync(basket.Id);
+
+        }
+    }
+}
