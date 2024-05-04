c33d297fdbd5578ec036c1d589d4ea9b4fcab087
shehabsameh1234 <shehabsameh987123@gmail.com>
2024-05-04 22:57:38 +0300

    order Module Entities

---
+++ Talabat.Core/Entities/Order-Aggregate/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+    public class Order:BaseEntity
+    {
+        public string BuyerEmail { get; set; } = null!;
+        public DateTimeOffset OrederDate { get; set; } = DateTimeOffset.UtcNow;
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public Address ShippingAddress { get; set; }= null!;
+        //public int DeliveryMethodId { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; } = null!;
+        public ICollection<OrderItem> OrderItems { get; set; } = new HashSet<OrderItem>();
+        //will calculate before  DeliveryMethod.Cost
+        public decimal SubTotal { get; set; }
+        //public decimal total { get => SubTotal + DeliveryMethod.Cost; }
+        //[NotMapped]
+        //public decimal Total => SubTotal + DeliveryMethod.Cost;
+        //==
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Cost;
+
+        public string PaymentIntitId { get; set; } = string.Empty;
+    }
+}
