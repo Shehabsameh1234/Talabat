510fe4a2f756ffb25038eeece6b612ff776b05f5
shehabsameh1234 <shehabsameh987123@gmail.com>
2024-05-19 21:47:09 +0300

    Create or update payment intent service and endpoint

---
+++ Talabat.APIs/Controllers/PaymentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.Errors;
+using Talabat.Core.Entities;
+using Talabat.Core.Service.Contract;
+
+namespace Talabat.APIs.Controllers
+{
+
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public class PaymentController : BaseApiController
+	{
+		private readonly IPaymentService _paymentService;
+
+		public PaymentController(IPaymentService paymentService)
+        {
+			_paymentService = paymentService;
+		}
+
+		[ProducesResponseType(typeof(CustomerBasket), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApisResponse), StatusCodes.Status400BadRequest)]
+		[HttpGet("{basketId}")]
+		public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+		{
+			var basket =await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+			if (basket is null) return BadRequest(new ApisResponse(400,"An Error In Your Basket"));
+			return Ok(basket);
+		} 
+
+    }
+}
--- Talabat.APIs/Dtos/CustomerBasketDto.cs
+++ Talabat.APIs/Dtos/CustomerBasketDto.cs
@@ -5,8 +5,12 @@ namespace Talabat.APIs.Dtos
 {
     public class CustomerBasketDto
     {
-        [Required]
-        public string Id { get; set; }
-        public List<BasketItemsDto> Items { get; set; }
-    }
+		[Required]
+		public string Id { get; set; }
+		public List<BasketItems> Items { get; set; }
+		public string? PaymentIntentId { get; set; } = null!;
+		public string? ClientSecret { get; set; } = null!;
+		public decimal ShippingPrice { get; set; }
+		public int? DeliveryMethodId { get; set; }
+	}
 }
